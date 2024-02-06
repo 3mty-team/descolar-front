ee1e7910b64fef98b5f1ada9e1e01e99b5786d2e
tosu! <theo.constant42@gmail.com>
2024-02-06 12:48:45 +0100

    Basic setup

--- app/docs/run.md
+++ app/docs/run.md
@@ -1,3 +1,5 @@
 # Comment lancer le projet
 
 Pour lancer l'application, **lancer l'appareil virtuel**, puis utiliser la commande `flutter run`
+
+Pour reload les dépendances (`pubspec.yaml`), faire la commande `flutter pub get` (automatique quand on save le fichier avec vscode)
--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,5 +1,5 @@
-import 'package:app/src/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,6 +10,34 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: true, // turn to false before release
       theme: ThemeData(fontFamily: 'Roboto'),
       home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: MyAppBar(),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  @override
+  final Size preferredSize;
+
+  const MyAppBar({super.key, this.preferredSize = const Size.fromHeight(50)});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color(0xFF8B1538),
+      title: SvgPicture.asset('assets/icons/descolar.svg'),
+      centerTitle: true,
     );
   }
 }
