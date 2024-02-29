44f52d0d3b8126cd16c63eba76fe506c63643e8e
Theo Constant <theo.constant@se.linedata.com>
2024-02-29 14:39:59 +0100

    Clean Architecture

---
+++ lib/src/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true, // TODO : Change to false for release
+      title: 'Descolar',
+      theme: ThemeData(// TODO
+
+          ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
