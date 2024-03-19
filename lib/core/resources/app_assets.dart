35f8f84fd579232af0fde50642edd0c385141cfc
Zakiryo <yohan.rudny@outlook.fr>
2024-03-19 12:20:53 +0100

    Started basic navigation bar

--- lib/core/resources/app_assets.dart
+++ lib/core/resources/app_assets.dart
@@ -7,5 +7,10 @@ class AppAssets {
   static const imgPath = '$basePath/images';
 
   static final descolarLogoSvg = SvgPicture.asset('$iconPath/descolar.svg');
+  static final homeIcon = SvgPicture.asset('$iconPath/home_icon.svg');
+  static final messageIcon = SvgPicture.asset('$iconPath/message_icon.svg');
+  static final newPostIcon = SvgPicture.asset('$iconPath/new_post_icon.svg');
+  static final profilIcon = SvgPicture.asset('$iconPath/profil_icon.svg');
+  static final searchIcon = SvgPicture.asset('$iconPath/search_icon.svg');
   
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:descolar_front/config/routes/app_routes.dart';
 import 'package:descolar_front/config/themes/app_themes.dart';
+import 'package:descolar_front/core/resources/app_assets.dart';
+import 'package:descolar_front/core/resources/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:descolar_front/screens/splash_screen.dart';
@@ -22,7 +25,7 @@ class MyApp extends StatelessWidget {
       //onGenerateRoute: AppRoutes.onGenerateRoutes, // /!\ MAYBE USELESS WITH PROVIDERS /!\
       title: 'Descolar',
       theme: theme(),
-      home: const SplashScreen(),
+      home: const Home(),
     );
   }
 }
@@ -36,6 +39,48 @@ class Home extends StatelessWidget {
       // TODO : Custom Desolar appbar
       appBar: AppBar(
         title: const Text('Descolar'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: AppColors.primary,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: AppAssets.homeIcon,
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: AppAssets.searchIcon,
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: AppAssets.newPostIcon,
+            label: 'New Post',
+          ),
+          BottomNavigationBarItem(
+            icon: AppAssets.profilIcon,
+            label: 'Profil',
+          ),
+          BottomNavigationBarItem(
+            icon: AppAssets.messageIcon,
+            label: 'Messages',
+          ),
+        ],
+        onTap: (value) {
+          switch (value) {
+            case 0:
+              Navigator.pushNamed(context, '/'); //Must be replaced by the true routes when created
+            case 1:
+              Navigator.pushNamed(context, '/');
+            case 2:
+              Navigator.pushNamed(context, '/');
+            case 3:
+              Navigator.pushNamed(context, '/');
+            case 4:
+              Navigator.pushNamed(context, '/');
+          }
+        },
       ),
     );
   }
