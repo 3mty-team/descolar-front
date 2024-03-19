40939158e350492d7b4e819b99948ecfaa261398
Zakiryo <yohan.rudny@outlook.fr>
2024-03-19 18:31:14 +0100

    Partially fixed the navbar

--- lib/core/components/navigation_bar.dart
+++ lib/core/components/navigation_bar.dart
@@ -6,59 +6,64 @@ class DescolarNavigationBar {
     return child;
   }
 
-  static Container mainNavBar(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(image: AppAssets.backgroundNav, fit: BoxFit.fill),
-      ),
-      child: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: Colors.transparent.withOpacity(0),
-        elevation: 0,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: AppAssets.homeIcon,
-            label: 'Home',
+  static Stack mainNavBar(BuildContext context) {
+    return Stack(
+      alignment: const Alignment(0, 1.5),
+      children: [
+        AppAssets.backgroundNav,
+        Theme(
+          data: ThemeData(splashColor: Colors.transparent),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            iconSize: 24,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: AppAssets.homeIcon,
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: AppAssets.searchIcon,
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: AppAssets.newPostIcon,
+                label: 'New Post',
+              ),
+              BottomNavigationBarItem(
+                icon: AppAssets.profilIcon,
+                label: 'Profil',
+              ),
+              BottomNavigationBarItem(
+                icon: AppAssets.messageIcon,
+                label: 'Messages',
+              ),
+            ],
+            onTap: (value) {
+              switch (value) {
+                case 0:
+                  Navigator.pushNamed(context, '/home');
+                  break;
+                case 1:
+                  Navigator.pushNamed(context, '/search');
+                  break;
+                case 2:
+                  Navigator.pushNamed(context, '/newPost');
+                  break;
+                case 3:
+                  Navigator.pushNamed(context, '/myProfil');
+                  break;
+                case 4:
+                  Navigator.pushNamed(context, '/messages');
+                  break;
+              }
+            },
           ),
-          BottomNavigationBarItem(
-            icon: AppAssets.searchIcon,
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: AppAssets.newPostIcon,
-            label: 'New Post',
-          ),
-          BottomNavigationBarItem(
-            icon: AppAssets.profilIcon,
-            label: 'Profil',
-          ),
-          BottomNavigationBarItem(
-            icon: AppAssets.messageIcon,
-            label: 'Messages',
-          ),
-        ],
-        onTap: (value) {
-          switch (value) {
-            case 0:
-              Navigator.pushNamed(context, '/home');
-              break;
-            case 1:
-              Navigator.pushNamed(context, '/search');
-              break;
-            case 2:
-              Navigator.pushNamed(context, '/newPost');
-              break;
-            case 3:
-              Navigator.pushNamed(context, '/myProfil');
-              break;
-            case 4:
-              Navigator.pushNamed(context, '/messages');
-              break;
-          }
-        },
-      ),
+        ),
+      ],
     );
   }
 }
