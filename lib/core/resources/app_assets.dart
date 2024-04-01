11ab063e88a84d8d9d0c19e3461c516ba2756a7f
Zakiryo <yohan.rudny@outlook.fr>
2024-04-01 16:15:17 +0200

    Post page completed (image adding and last modifications)

--- lib/core/resources/app_assets.dart
+++ lib/core/resources/app_assets.dart
@@ -6,8 +6,6 @@ class AppAssets {
   static const imgPath = '$basePath/images';
 
   static final descolarLogoSvg = SvgPicture.asset('$iconPath/descolar.svg');
-  static final sampleProfilIcon = SvgPicture.asset('$iconPath/profil_sample.svg');
-
   static final homeIcon = SvgPicture.asset('$iconPath/home_icon.svg');
   static final messageIcon = SvgPicture.asset('$iconPath/message_icon.svg');
   static final newPostIcon = SvgPicture.asset('$iconPath/new_post_icon.svg');
@@ -16,6 +14,5 @@ class AppAssets {
   static final backgroundNav = SvgPicture.asset('$imgPath/backgroundNavBar.svg');
   static final settingsIcon = SvgPicture.asset('$iconPath/settings-icon.svg');
   static final backIcon = SvgPicture.asset('$iconPath/back-icon.svg');
-  static final closeIcon = SvgPicture.asset('$iconPath/close_icon.svg');
   static final addImageIcon = SvgPicture.asset('$iconPath/add_image_icon.svg');
 }
