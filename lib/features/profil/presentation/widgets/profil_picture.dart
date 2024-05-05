a61f23ee417ff248720b36984bcc75e4352510f8
tosu! <theo.constant42@gmail.com>
2024-05-05 02:18:36 +0200

    Default profil pic when bad path

--- lib/features/profil/presentation/widgets/profil_picture.dart
+++ lib/features/profil/presentation/widgets/profil_picture.dart
@@ -13,14 +13,33 @@ class ProfilPicture extends StatefulWidget {
 }
 
 class _ProfilPictureState extends State<ProfilPicture> {
+
+  String? getValidImagePath() {
+    if (widget.imagePath != null) {
+      if (widget.imagePath!.isEmpty) {
+        return null;
+      }
+      if (widget.imagePath!.length <= 64) {
+        return null;
+      }
+      if (widget.imagePath!.substring(0, 63) != 'https://internal-api.descolar.fr/v1/App/Adapters/Media/Storage/') {
+        return null;
+      }
+    }
+
+    return widget.imagePath;
+  }
+
   @override
   Widget build(BuildContext context) {
+    String? validPath = getValidImagePath();
+
     return CircleAvatar(
       radius: widget.radius + (widget.borderWidth ?? 0),
       backgroundColor: AppColors.white,
       child: CircleAvatar(
         radius: widget.radius,
-        foregroundImage: widget.imagePath != null
+        foregroundImage: validPath != null
             ? Image.network(
                 widget.imagePath!,
               ).image
